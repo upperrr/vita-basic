cef38a062f25eb9f90052e16504dbcf63d1fdb87
sypark <upperrr@gmail.com>
2024-05-30 13:36:52 +0900

    :sparkles: feat: computed

---
+++ eventHandler.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <!-- 인라인 핸들러(v-click) 사용 -->
+      <button v-on:click="count++">인라인 핸들러</button>
+      <h1> {{ count }}</h1> <!-- 텍스트 보간법 호출 -->
+  
+      <!-- 메서드 핸들러 -->
+      <button v-on:click="changeName">메서드 핸들러</button>
+      <h1> {{ name }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data () {
+      
+      return {
+        count: 0,
+        name: 'Vue.js',
+      }
+    },
+      methods: {
+        changeName() {
+          this.name = '변경된 텍스트 데이터 입니다.'
+        },
+      },
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <!-- 인라인 핸들러(v-click) 사용 -->
-    <button v-on:click="count++">인라인 핸들러</button>
-    <h1> {{ count }}</h1> <!-- 텍스트 보간법 호출 -->
+    <h1>{{ text }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
+    <h1>changeText 함수 호출 값: {{ changeText() }}</h1>
 
-    <!-- 메서드 핸들러 -->
-    <button v-on:click="changeName">메서드 핸들러</button>
-    <h1> {{ name }}</h1>
+    <h2>{{ computedText }}</h2>
+    <h2>{{ computedText }}</h2>
+    <h2>{{ computedText }}</h2>
   </div>
 </template>
 
@@ -15,15 +16,28 @@ export default {
   data () {
     
     return {
-      count: 0,
-      name: 'Vue.js',
+      text: 'Computed 테스트 데이터',
     }
   },
-    methods: {
-      changeName() {
-        this.name = '변경된 텍스트 데이터 입니다.'
-      },
+  // computed : 모양은 함수 이지만 data 취급을 합니다.
+  // method 부분에 선언된 함수와 동일한 로직일 때
+  // 캐싱 기능이 없는 methods는 호출될 때마다 console 값이 출력 되었습니다.
+  // 반면에 computed는 캐싱 기능이 없기 때문에 methods와 어떤 차이점이 있는지
+  // 한번 주의깊게 살펴보는 것이 포인트 입니다.
+  computed: {
+    computedText() {
+      console.log('Computed 기능을 생성하였습니다.')
+      return this.text.split("").reverse().join('')
     },
+  },
+  methods: {
+    changeText() {
+      console.log("함수 호출");
+      console.log(this.text);
+
+      return this.text.split("").reverse().join('')
+    },
+  }
 }
 </script>
 
